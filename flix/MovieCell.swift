9ce96ed033f1f6c1a85e094cb9a4263cd530530d
KhuziamaR <krehman000@citymail.cuny.edu>
2021-02-26 09:52:36 -0500

    new updated project

---
+++ flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Khuziama Rehman on 2/26/21.
+//  Copyright © 2021 Khuziama Rehman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Khuziama Rehman on 2/26/21.
+//  Copyright © 2021 Khuziama Rehman. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
